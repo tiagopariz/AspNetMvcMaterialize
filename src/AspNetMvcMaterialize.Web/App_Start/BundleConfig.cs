7eeab63e93ef11731285c59bb57a911e214f347d
tiagopariz <tiagopariz@gmail.com>
2018-04-28 10:46:22 -0300

    Materialize

--- src/AspNetMvcMaterialize.Web/App_Start/BundleConfig.cs
+++ src/AspNetMvcMaterialize.Web/App_Start/BundleConfig.cs
@@ -1,31 +1,19 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace AspNetMvcMaterialize.Web
 {
     public class BundleConfig
     {
-        // Para obter mais informações sobre o agrupamento, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/js/jquery").Include(
+                        "~/js/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/js/materialize").Include(
+                      "~/js/materialize.js"));
 
-            // Use a versão em desenvolvimento do Modernizr para desenvolver e aprender. Em seguida, quando estiver
-            // pronto para a produção, utilize a ferramenta de build em https://modernizr.com para escolher somente os testes que precisa.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
-                      "~/Scripts/materialize/materialize.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/materialize/css/materialize.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/css").Include(
+                      "~/css/materialize.css"));
         }
     }
 }
